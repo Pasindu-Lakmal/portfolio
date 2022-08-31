52fd9ddf68d8ede7cca9a19a4c53ed85dd66b694
Pasindu-Lakmal <pasindu1llak@gmail.com>
2022-08-31 17:12:18 +0530

    initializefile

---
+++ src/screens/HomePage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+const HomePage = () => {
+  return (
+    <div>
+      <h1>this is Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
